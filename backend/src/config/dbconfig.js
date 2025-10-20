9d18a631b538100e9a31051f18e74a494da88c44
Isagc <isabelagrisalescastrillon@gmail.com>
2025-10-20 14:46:09 -0500

    nueva config

--- backend/src/config/dbconfig.js
+++ backend/src/config/dbconfig.js
@@ -1,10 +1,18 @@
-//creamos la conexion a la base de datos con zequelize
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("saturday", "root", "admin", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false, 
+  }
+);
 
 export default sequelize;
